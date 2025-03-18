a8443fb0092c6d867ce26e7d826ec178b3fcb058
ganeshshejwal <ganesh.shejwal@bnt-soft.com>
2025-03-18 14:37:23 +0530

    Add contoller methods

--- src/main/java/com/cennox/rcp/controller/RcpController.java
+++ src/main/java/com/cennox/rcp/controller/RcpController.java
@@ -1,18 +1,9 @@
 package com.cennox.rcp.controller;
 
-import com.cennox.rcp.entity.Device;
-import com.cennox.rcp.service.RcpService;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,10 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
-
 import com.cennox.rcp.entity.Acquirer;
+import com.cennox.rcp.entity.Device;
 import com.cennox.rcp.service.RcpService;
 
 @RestController
@@ -37,6 +26,32 @@ public class RcpController {
 
     public RcpController(RcpService rcpService) {
         this.rcpService = rcpService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Device> createDevice(@RequestBody Device device) {
+        return ResponseEntity.ok(rcpService.createDevice(device));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Device> getDeviceById(@PathVariable UUID id) {
+        Device device = rcpService.getDeviceById(id);
+        return ResponseEntity.ok(device);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Device>> getAllDevices() {
+        return ResponseEntity.ok(rcpService.getAllDevices());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Device> updateDevice(@PathVariable UUID id, @RequestBody Device device) {
+        return ResponseEntity.ok(rcpService.updateDevice(id, device));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteDevice(@PathVariable UUID id) {
+        return ResponseEntity.ok(rcpService.deleteDevice(id));
     }
 
     @PostMapping("/acquirer/create")
