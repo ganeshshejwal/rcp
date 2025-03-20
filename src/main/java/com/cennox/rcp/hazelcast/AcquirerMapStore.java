7625ef2126f414b336fa0aaf4a4a5cd99a279eea
ganeshshejwal <ganesh.shejwal@bnt-soft.com>
2025-03-20 10:55:48 +0530

    Add mapstore config for entities

---
+++ src/main/java/com/cennox/rcp/hazelcast/AcquirerMapStore.java
@@ -0,0 +1,76 @@
+package com.cennox.rcp.hazelcast;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.cennox.rcp.entity.Acquirer;
+import com.cennox.rcp.repository.AcquirerRepository;
+import com.hazelcast.map.MapStore;
+
+@Component
+public class AcquirerMapStore implements MapStore<UUID, Acquirer> {
+    
+    @Lazy
+    private AcquirerRepository acquirerRepository;
+
+    public AcquirerMapStore(AcquirerRepository acquirerRepository) {
+        this.acquirerRepository = acquirerRepository;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(AcquirerMapStore.class);
+
+    @Override
+    public Acquirer load(UUID key) {
+        logger.info("Fetching data for key: {} from DB", key);
+        return acquirerRepository.findById(key).orElse(null);
+    }
+
+    @Override
+    public Map<UUID, Acquirer> loadAll(Collection<UUID> keys) {
+        Map<UUID, Acquirer> map = new HashMap<>();
+        List<Acquirer> acquirers = acquirerRepository.findAllById(keys);
+        for (Acquirer acquirer : acquirers) {
+            map.put(acquirer.getAcquirerId(), acquirer);
+        }
+        logger.info("Fetching multiple keys from DB: {}", keys);
+        return map;
+    }
+
+    @Override
+    public Iterable<UUID> loadAllKeys() {
+        List<UUID> allKeys = acquirerRepository.findAll().stream().map(Acquirer::getAcquirerId).toList();
+        logger.info("Keys loaded: {}", allKeys);
+        return allKeys;
+    }
+
+    @Override
+    public void store(UUID key, Acquirer value) {
+        logger.info("Storing data for key: {} in DB", key);
+        acquirerRepository.save(value);
+    }
+
+    @Override
+    public void storeAll(Map<UUID, Acquirer> map) {
+        acquirerRepository.saveAll(map.values());
+    }
+
+    @Override
+    public void delete(UUID key) {
+        logger.info("Deleting data for key: {} from DB", key);
+        acquirerRepository.deleteById(key);
+    }
+
+    @Override
+    public void deleteAll(Collection<UUID> keys) {
+        keys.forEach(acquirerRepository::deleteById);
+    }
+}
+
---
+++ src/main/java/com/cennox/rcp/hazelcast/DeviceMapStore.java
@@ -0,0 +1,76 @@
+package com.cennox.rcp.hazelcast;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.cennox.rcp.entity.Device;
+import com.cennox.rcp.repository.DeviceRepository;
+import com.hazelcast.map.MapStore;
+
+@Component
+public class DeviceMapStore implements MapStore<UUID, Device> {
+    
+    @Lazy
+    private DeviceRepository deviceRepository;
+
+    public DeviceMapStore(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
+
+    private Logger logger = LoggerFactory.getLogger(DeviceMapStore.class);
+
+    @Override
+    public Device load(UUID key) {
+        logger.info("Fetching data for key: {} from DB", key);
+        return deviceRepository.findById(key).orElse(null);
+    }
+
+    @Override
+    public Map<UUID, Device> loadAll(Collection<UUID> keys) {
+        Map<UUID, Device> map = new HashMap<>();
+        List<Device> devices = deviceRepository.findAllById(keys);
+        for (Device device : devices) {
+            map.put(device.getId(), device);
+        }
+        logger.info("Fetching multiple keys from DB: {}", keys);
+        return map;
+    }
+
+    @Override
+    public Iterable<UUID> loadAllKeys() {
+        List<UUID> allKeys = deviceRepository.findAll().stream().map(Device::getId).toList();
+        logger.info("Keys loaded: {}", allKeys);
+        return allKeys;
+    }
+
+    @Override
+    public void store(UUID key, Device value) {
+        logger.info("Storing data for key: {} in DB", key);
+        deviceRepository.save(value);
+    }
+
+    @Override
+    public void storeAll(Map<UUID, Device> map) {
+        deviceRepository.saveAll(map.values());
+    }
+
+    @Override
+    public void delete(UUID key) {
+        logger.info("Deleting data for key: {} from DB", key);
+        deviceRepository.deleteById(key);
+    }
+
+    @Override
+    public void deleteAll(Collection<UUID> keys) {
+        keys.forEach(deviceRepository::deleteById);
+    }
+}
+
